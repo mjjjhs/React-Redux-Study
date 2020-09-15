107ae6ad7b84a088c6a0002bc77f144c28e7b691
jihoon.moon <mjjjhs@gmail.com>
2020-09-15 18:05:53 +0900

    리덕스 기본 스터디

---
+++ src/react-redux/styles/UserStyle.ts
@@ -0,0 +1,9 @@
+import css from 'styled-jsx/css';
+export default css`
+    div {
+        display:inline-flex;
+        justify-content: space-between;
+        min-width: 250px;
+        margin-bottom: 20px;
+    }
+`;
