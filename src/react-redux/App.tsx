e684b002b47707db2e2acae7f5226147d3b76fe3
jihoon.moon <mjjjhs@gmail.com>
2021-02-03 02:47:11 +0900

    설정변경

--- src/react-redux/App.tsx
+++ src/react-redux/App.tsx
@@ -5,6 +5,13 @@ import User from './components/User';
 import {IAddUser, IUserInfo} from "./interfaces/User";
 import UserStyle from "./styles/UserStyle";
 import GlobalStyle from "./styles/GlobalStyle";
+
+const _JSXStyle = require('styled-jsx/style').default;
+
+if (typeof global !== 'undefined') {
+    Object.assign(global, { _JSXStyle });
+}
+
 let curId = 1;
 
 function App({users, addUser}: {users: IUserInfo[], addUser: IAddUser}) {
